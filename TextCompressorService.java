ea2c7ddf5a0acf35d41d77b6f065630790db8687
ksrilakshmi5 <131759753+ksrilakshmi5@users.noreply.github.com>
2024-11-21 12:19:14 +0530

    Update TextCompressorService.java

--- TextCompressorService.java
+++ TextCompressorService.java
@@ -1,23 +1,31 @@
 package com.example.textcompressor.service;
 
 import com.example.textcompressor.model.TextRequest;
-import com.example.textcompressor.repository.TextCompressorRepository;
 import com.example.textcompressor.util.HuffmanCoding;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class TextCompressorService {
     
+    private List<TextRequest> textRequests = new ArrayList<>();
+
+    // Method to save a text request and compress it
     public TextRequest saveTextRequest(TextRequest request) {
+        
         String encoded = HuffmanCoding.encode(request.getText());
         request.setEncodedText(encoded);
-        return repository.save(request);
+
+      
+        textRequests.add(request);
+
+        return request;
     }
 
+    // Method to get all saved requests
     public List<TextRequest> getAllRequests() {
-        return repository.findAll();
+        return textRequests;
     }
 }
